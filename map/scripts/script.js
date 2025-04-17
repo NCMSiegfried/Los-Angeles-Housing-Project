d773ed70590636777ea32c030714f537ba7b3e46
Nikko <nikkosiegfried@gmail.com>
2025-04-17 15:28:09 -0500

    add geojson for map points

--- map/scripts/script.js
+++ map/scripts/script.js
@@ -1,10 +1,39 @@
 const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://demotiles.maplibre.org/style.json',
-    center: [-74.006, 40.7128], // New York City coordinates
-    zoom: 10
+    style: 'https://api.maptiler.com/maps/topo-v2-dark/style.json?key=IO8n64EFzCLGpJrn7JW3',
+    center: [-118.2426, 34.0549], // Los Angeles coordinates
+    zoom: 8,
+    minZoom: 7
 });
 
-new maplibregl.Marker()
-    .setLngLat([-74.006, 40.7128])
-    .addTo(map);
+function rotateAndZoom() {
+    map.easeTo({
+        zoom: 10,      // Zoom in
+        duration: 3500, // Smooth transition over 5 seconds
+        pitch: 40
+    });
+}
+
+map.on('load', function () {
+    rotateAndZoom();
+    fetch('../data/Los_Angeles_Sold.geojson')
+    .then(response => response.json())
+    .then(data => {
+      map.addSource('my-geojson', {
+        type: 'geojson',
+        data: data
+      });
+
+      map.addLayer({
+        id: 'geojson-layer',
+        type: 'circle',
+        source: 'my-geojson',
+        paint: {
+          'circle-radius': 5,
+          'circle-color': '#007cbf'
+        }
+      });
+    });
+});
+
+
